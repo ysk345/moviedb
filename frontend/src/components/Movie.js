1ee821be7f2f0fdd8e65a5aa9625b97ae1e77261
y.abz <y.abaszadeh@airinmedia.com>
2023-11-18 11:02:11 -0500

    Seperate the components

--- frontend/src/components/Movie.js
+++ frontend/src/components/Movie.js
@@ -1,22 +1,40 @@
-import EditMovie from "./EditMovie";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-function Movie(props) {
+const Movie = ({ movie, openDeleteModal }) => {
+  const navigate = useNavigate();
+
+  const navigateToMovieDetail = () => {
+    navigate(`/movie/${movie._id}`);
+  };
   return (
-    <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 mb-4">
-      <img
-        className="block mx-auto h-24  sm:mx-0 sm:shrink-0"
-        src={props.img}
-        alt="movie-poster"
-      />
-      <div className="text-center space-y-2 sm:text-left">
-        <div className="space-y-0.5">
-          <p className="text-lg text-black font-semibold">{props.name}</p>
-          <p className="text-slate-500 font-medium">{props.year}</p>
-        </div>
-        <EditMovie/>
-      </div>
-    </div>
+    <tr
+      className="cursor-pointer hover:shadow-lg transition-transform duration-200"
+      onClick={navigateToMovieDetail}
+    >
+      <td>{movie.title}</td>
+      <td>{movie.description}</td>
+      <td>
+        <img
+          src={movie.imgURL}
+          alt={movie.title}
+          style={{ maxWidth: "100px" }}
+        />
+      </td>
+      <td>{movie.year}</td>
+      <td>{movie.genre}</td>
+      <td>{movie.director}</td>
+      <td>
+        <Link to={"/edit/" + movie._id}>Edit</Link>
+        <button
+          className="bg-transparent text-red-500 border-none w-9 h-9 text-sm flex items-center justify-center hover:bg-red-100 focus:outline-none"
+          onClick={() => openDeleteModal(movie)}
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
   );
-}
+};
 
 export default Movie;
