c872ec1e627ab2192e33171f9930d6cdae37e96c
y.abz <y.abaszadeh@airinmedia.com>
2023-11-18 11:09:21 -0500

    Fix click on each row

--- frontend/src/components/Movie.js
+++ frontend/src/components/Movie.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Movie = ({ movie, openDeleteModal }) => {
   const navigate = useNavigate();
 
   const navigateToMovieDetail = () => {
     navigate(`/movie/${movie._id}`);
+  };
+  const handleDeleteClick = (e) => {
+    e.stopPropagation(); // Prevent row click event
+    openDeleteModal();
+  };
+  const handleEditClick = (e) => {
+    e.stopPropagation(); // Prevent row click event
+    navigate(`/edit/${movie._id}`);
   };
   return (
     <tr
@@ -25,10 +33,15 @@ const Movie = ({ movie, openDeleteModal }) => {
       <td>{movie.genre}</td>
       <td>{movie.director}</td>
       <td>
-        <Link to={"/edit/" + movie._id}>Edit</Link>
+        <button
+          className="bg-transparent text-blue-500 border-none w-9 h-9 text-sm flex items-center justify-center"
+          onClick={handleEditClick}
+        >
+          Edit
+        </button>
         <button
           className="bg-transparent text-red-500 border-none w-9 h-9 text-sm flex items-center justify-center hover:bg-red-100 focus:outline-none"
-          onClick={() => openDeleteModal(movie)}
+          onClick={handleDeleteClick}
         >
           Delete
         </button>
