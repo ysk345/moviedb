212ec6e256b252da0b2189a7551c3843d32b873e
y.abz <y.abaszadeh@airinmedia.com>
2023-11-18 11:02:24 -0500

    Add detail page

--- frontend/src/pages/MovieDetails.js
+++ frontend/src/pages/MovieDetails.js
@@ -1,20 +1,37 @@
+// MovieDetail.js
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
-export default function MovieDetails(){
-    const params = useParams();
-    let movieData = null;
+const MovieDetail = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
 
-    return (
-        <>
-        <h1>Movie Details</h1>
-        <p>Movie ID: {params.id}</p>
-        {movieData ? (
-            <>
-            <img src={movieData.img} alt={movieData.name} width="200"/>
-            <h2>{movieData.name}{movieData.year}</h2>
-            </>
-        ) : (<p>Not Found</p>)
-        }
-        </>
-    );
-}
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8081/api/movies/${id}`)
+      .then((res) => {
+        setMovie(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">{movie.title}</h1>
+      <img
+        src={movie.imgURL}
+        alt={movie.title}
+        className="w-full max-w-md my-4"
+      />
+      <p>{movie.description}</p>
+      {/* Add more details as needed */}
+    </div>
+  );
+};
+
+export default MovieDetail;
